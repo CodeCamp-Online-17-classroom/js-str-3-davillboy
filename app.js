b55894b54f5f2f10598eeeefa8030bc075edfb01
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 21:23:39 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // write code here
+          function ucFirst(str) {
+            if (!str) return str; 
+            return str[0].toUpperCase() + str.slice(1);
+          }
+          
+          let str = 'javascript';
+          console.log(ucFirst(str)); 
